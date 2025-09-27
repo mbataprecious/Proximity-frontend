81299ea4c6be3967d1356d8ed5a67610b5483507
Mbata Precious <mbataprecious9@gmail.com>
2025-09-27 23:58:51 +0100

    added for migration

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
+import HeroSection from "@/components/landingSections/HeroSection";
+
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24"></main>
-  );
+  return <HeroSection />;
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,7 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
+  darkMode: ["class"],
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
@@ -25,15 +26,71 @@ const config: Config = {
         sans: ["Inter", "sans-serif"],
       },
       colors: {
-        primary: "#096DD9",
-        secondary: "#F7931E",
+        primary: {
+          DEFAULT: "#096DD9",
+          foreground: "hsl(var(--primary-foreground))",
+        },
+        secondary: {
+          DEFAULT: "#F7931E",
+          foreground: "hsl(var(--secondary-foreground))",
+        },
+        background: "hsl(var(--background))",
+        foreground: "hsl(var(--foreground))",
+        card: {
+          DEFAULT: "hsl(var(--card))",
+          foreground: "hsl(var(--card-foreground))",
+        },
+        popover: {
+          DEFAULT: "hsl(var(--popover))",
+          foreground: "hsl(var(--popover-foreground))",
+        },
+        muted: {
+          DEFAULT: "hsl(var(--muted))",
+          foreground: "hsl(var(--muted-foreground))",
+        },
+        accent: {
+          DEFAULT: "hsl(var(--accent))",
+          foreground: "hsl(var(--accent-foreground))",
+        },
+        destructive: {
+          DEFAULT: "hsl(var(--destructive))",
+          foreground: "hsl(var(--destructive-foreground))",
+        },
+        border: "hsl(var(--border))",
+        input: "hsl(var(--input))",
+        ring: "hsl(var(--ring))",
+        chart: {
+          "1": "hsl(var(--chart-1))",
+          "2": "hsl(var(--chart-2))",
+          "3": "hsl(var(--chart-3))",
+          "4": "hsl(var(--chart-4))",
+          "5": "hsl(var(--chart-5))",
+        },
+        mariner: {
+          "50": "#edfaff",
+          "100": "#d7f2ff",
+          "200": "#b9e9ff",
+          "300": "#88deff",
+          "400": "#50c9ff",
+          "500": "#28acff",
+          "600": "#118fff",
+          "700": "#096dd9",
+          "800": "#0f5ebe",
+          "900": "#135195",
+          "950": "#11325a",
+        },
       },
       boxShadow: {
         "custom-purple": "0 8px 16px #7B68EE66",
         "light-shade": "0 6px 12px 0 rgba(27, 105, 253, 0.27)",
       },
+      borderRadius: {
+        lg: "var(--radius)",
+        md: "calc(var(--radius) - 2px)",
+        sm: "calc(var(--radius) - 4px)",
+      },
     },
   },
-  plugins: [],
+  plugins: [require("tailwindcss-animate")],
 };
 export default config;
